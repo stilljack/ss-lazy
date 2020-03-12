538cc4a233a339770c4a335b3f0177c44a2e9f41
Jack Seymour <stilljack@gmail.com>
2020-03-11 19:35:45 -0700

    i have no idea why this doesn't work

--- server.js
+++ server.js
@@ -11,24 +11,15 @@ const path = require('path')
 // in the dist directory
 const app_path = "dist/ss-lazy-eval"
 
-app.use('/', express.static(path.join(__dirname,app_path)))
-  .get('*',(req, res)=>restore.sendfile(path.join(__dirname,app_path,
-    "index.html")))
+
+app.listen(3000);
+app.use(express.static(path.join(__dirname,app_path)))
   .listen(port,()=>console.log(`listening on ${port}`));
-// Start the app by listening on the default
-// // Heroku port
-// app.listen(process.env.PORT || port);git addd
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'dist/ss-lazy/eval/index.html'));
-// });
-// app.listen(port, function() {
-//     console.log('server running on localhost:' + port);
-// });
-// app.use(express.static(path.join(__dirname, 'dist')));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// //
-// // app.use('/api', api);
+
+
+
+// Start the app by listening on the default Heroku port
+
 //
 
 //
