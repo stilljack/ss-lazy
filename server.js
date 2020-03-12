05f4f5d791d8a76f9370cb323ce0b44806cf1003
Jack Seymour <stilljack@gmail.com>
2020-03-11 19:06:06 -0700

    maybe this way?

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
 
-// const api = require('./server/routes/api');
+// const bodyParser = require('body-parser');
+// const path = require('path');
 
 const port = 8080;
+
+const express = require('express');
 const app = express();
+const path = require('path')
+// Run the app by serving the static files
+// in the dist directory
+const app_path = "dist/ss-lazy-eval"
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use('/', express.static(path.join(__dirname,app_path)))
+  .get('*',(req, res)=>restore.sendfile(path.join(__dirname,app_path,
+    +"index.html")))
+  .listen(port,()=>console.log(`listening on ${port}`));
+// Start the app by listening on the default
+// // Heroku port
+// app.listen(process.env.PORT || port);
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist/ss-lazy/eval/index.html'));
+// });
+// app.listen(port, function() {
+//     console.log('server running on localhost:' + port);
+// });
+// app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// //
+// // app.use('/api', api);
 //
-// app.use('/api', api);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-app.listen(port, function() {
-    console.log('server running on localhost:' + port);
-});
+//
+// app.listen(port, function() {
+//     console.log('server running on localhost:' + port);
+// });
